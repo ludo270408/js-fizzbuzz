f195a7547df3af72a0130c607e0df0e6f62e5ac6
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-28 23:58:43 +0100

    [UPTADE] General structue ready

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const fizzbuzz_dom = document.getElementById('fizzbuzz_dom');
 for(let i=1; i<=100; i++){
     let value;
     if(i%15==0){
-
+        value='FizzBuzz';
+        console.log('FizzBuzz');
     }
     else if(i%5==0){
-
+        value='FizzBuzz';
+        console.log('FizzBuzz');
     }
     else if(i%3==0){
-
+        value='FizzBuzz';
+        console.log('FizzBuzz');
     }
     else{
         value=i;
