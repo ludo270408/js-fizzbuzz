6a35e9f15ff8f1228215ff24cd335f155f629be0
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-29 00:52:42 +0100

    [UPTADE] Exercise done + problem solved

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const fizzbuzz_dom = document.getElementById('fizzbuzz-dom');
 
 for(let i=1; i<=100; i++){
-    let value;
+    let li;
     if(i%15==0){
-        value='FizzBuzz';
+        li='<li>FizzBuzz</li>';
         console.log('FizzBuzz');
     }
     else if(i%5==0){
-        value='Buzz';
+        li='<li>Buzz</li>';
         console.log('Buzz');
     }
     else if(i%3==0){
-        value='Fizz';
+        li='<li>Fizz</li>';
         console.log('Fizz');
     }
     else{
-        value=i;
+       li ='<li>$(i)</li>';
         console.log(i);
     }
-    fizzbuzz_dom.innerHTML +=value;
+    
+    fizzbuzz_dom.innerHTML +=li;
 }
