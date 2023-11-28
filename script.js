494c35be0ab82b508e1873e4a5dadfbca825c947
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-28 23:54:19 +0100

    [UPTADE] Structure of the if

---
+++ script.js
@@ -0,0 +1,19 @@
+const fizzbuzz_dom = document.getElementById('fizzbuzz_dom');
+
+for(let i=1; i<=100; i++){
+    let value;
+    if(i%15==0){
+
+    }
+    else if(i%5==0){
+
+    }
+    else if(i%3==0){
+
+    }
+    else{
+        value=i;
+        console.log(i);
+    }
+
+}
