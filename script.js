e6c6041534f1c849f17e859c54cbfda5feaabaec
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-29 00:03:16 +0100

    [UPTADE] Done

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ for(let i=1; i<=100; i++){
         console.log('FizzBuzz');
     }
     else if(i%5==0){
-        value='FizzBuzz';
-        console.log('FizzBuzz');
+        value='Buzz';
+        console.log('Buzz');
     }
     else if(i%3==0){
-        value='FizzBuzz';
-        console.log('FizzBuzz');
+        value='Fizz';
+        console.log('Fizz');
     }
     else{
         value=i;
         console.log(i);
     }
-
+    fizzbuzz_dom.innerHTML +=value;
 }
